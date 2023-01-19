3f006014b2e7bdbba06b8b3fda13053ff8ce5895
Bernard Gatt <bernard@customer.io>
2023-01-19 14:10:31 +0100

    Updated queue endpoints and demo app config

--- src/services/settings.js
+++ src/services/settings.js
@@ -5,13 +5,13 @@ export const settings = {
     "local": "http://api.local.gist.build:83"
   },
   GIST_QUEUE_API_ENDPOINT: {
-    "prod": "https://queue.api.gist.build",
-    "dev": "https://queue.api.dev.gist.build",
+    "prod": "https://gist-queue-consumer-api.cloud.gist.build",
+    "dev": "https://gist-queue-consumer-api.cloud.dev.gist.build",
     "local": "http://api.local.gist.build:86"
   },
   GIST_VIEW_ENDPOINT: {
     "prod": "https://renderer.gist.build/1.0",
-    "dev": "https://code.gist.build/renderer/0.0.20",
+    "dev": "https://renderer.gist.build/1.0",
     "local": "http://app.local.gist.build:8080/web"
   }
 }
