996125ea3cdcf3ef6f1ebcc3c56cdadb76f49336
Steve Pope <stephen.pope@customer.io>
2023-04-13 11:04:19 -0230

    Code review feedback

--- src/services/queue-network.js
+++ src/services/queue-network.js
@@ -6,7 +6,7 @@ import { getUserToken } from "../managers/user-manager";
 export function NetworkInstance() {
   var headers = { 'X-CIO-Site-Id': `${Gist.config.siteId}`, "X-CIO-Datacenter": `${Gist.config.dataCenter}` };
   var userToken = getUserToken();
-  if (userToken !== undefined || userToken !== null) {
+  if (userToken != null) {
     headers['X-Gist-Encoded-User-Token'] = btoa(userToken);
     headers['X-Gist-User-Token'] = userToken;
   }
