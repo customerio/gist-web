fa28d0cc377d6aeee0f4777f252e14929df96b6e
Bernard Gatt <bernard@customer.io>
2025-02-18 13:13:00 +0100

    Added support for Queue V3

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,3 +1,7 @@
+import { getKeyFromLocalStore } from '../utilities/local-storage';
+import { log } from '../utilities/log';
+const userQueueVersionLocalStoreName = "gist.web.userQueueVersion";
+
 export const settings = {
   ENGINE_API_ENDPOINT: {
     "prod": "https://engine.api.gist.build",
@@ -9,9 +13,22 @@ export const settings = {
     "dev": "https://gist-queue-consumer-api.cloud.dev.gist.build",
     "local": "http://api.local.gist.build:86"
   },
+  GIST_QUEUE_V3_API_ENDPOINT: {
+    "prod": "https://consumer.cloud.gist.build",
+    "dev": "https://consumer.cloud.dev.gist.build",
+    "local": "http://api.local.gist.build:86"
+  },
   GIST_VIEW_ENDPOINT: {
     "prod": "https://renderer.gist.build/3.0",
     "dev": "https://renderer.gist.build/3.0",
     "local": "http://app.local.gist.build:8080/web"
+  },
+  getQueueAPIVersion: function(version) {
+    getKeyFromLocalStore(userQueueVersionLocalStoreName, version);
+    return version ?? "2";
+  },
+  setQueueAPIVersion: function(version) {
+    setKeyToLocalStore(userQueueVersionLocalStoreName, version);
+    log(`Set user queue version to "${version}"`);
   }
 }
