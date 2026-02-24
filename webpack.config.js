03c3c388152672459d61063fe87ac588cfd32059
Karn <karn@customer.io>
2026-02-24 16:39:01 +0530

    export default as Gist

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,18 @@
 module.exports = {
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   module: {
-    rules: [
-      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ },
-    ],
+    rules: [{ test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ }],
   },
   watchOptions: {
-    poll: 1000
+    poll: 1000,
   },
   output: {
-    library: 'Gist',
-    libraryTarget: 'umd',
-    filename: 'gist.js',
-    globalObject: 'this'
-  }
+    library: "Gist",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    filename: "gist.js",
+    globalObject: "this",
+  },
 };
